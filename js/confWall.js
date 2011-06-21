32e5161e4f352de85a77ac9ae9334bef11e86fce
Tiago Boldt Sousa <tiagoboldt@gmail.com>
2011-06-21 02:09:46 +0100

    increase delay

--- js/confWall.js
+++ js/confWall.js
@@ -24,14 +24,14 @@ function switchImage(){
 		previous = i-1;
 	}
 	
-	$('#img'+i).fadeIn(500);
-	$('#img'+previous).fadeOut(500, function() { $(this).remove(); });
+	$('#img'+i).fadeIn(1000);
+	$('#img'+previous).fadeOut(1000, function() { $(this).remove(); });
 
 	i += 1;
 	if(i === images.length){
 		i = 0;
 	}
-	setTimeout(switchImage, 5000);
+	setTimeout(switchImage, 7000);
 }
 
 
